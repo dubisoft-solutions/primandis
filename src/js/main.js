dc93d64bef758db09eaa3fe8573ce5a43b776a29
Ivan Dubynets <ivan.dubynets@gmail.com>
2022-09-18 12:30:56 +0200

    tiny slider integration

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 //= ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+//= ../../node_modules/tiny-slider/dist/min/tiny-slider.js
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    initFeedbackNotes();
+});
+
+
+function initFeedbackNotes() {
+    tns({
+        container: '.feedback-notes-slider',
+        items: 3,
+        slideBy: 1,
+        autoplay: true,
+        navAsThumbnails: true,
+        center: true,
+        controls: false,
+        autoplayButtonOutput: false,
+        navPosition: 'bottom',
+    });
+}
